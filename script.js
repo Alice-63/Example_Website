3089c6dc3c030411b00cc5c4220b1c6ddb9647a3
Alice-63 <alicetindag84@gmail.com>
2021-05-21 08:55:04 +0200

    ali

--- script.js
+++ script.js
@@ -10,13 +10,34 @@ function hinzufuegen(produkt, preis)
 {
     var kostenDiv =document.getElementById("gesamtKosten");
     document.getElementById("warenkorb").innerHTML+= ' <div>'+ zahl +'.' +' '+ produkt + ' , '+ preis + ' €,' + '</div>'; 
+    gesamtKosten += preis;
 
-    kostenDiv.innerHTML= "Guthaben: " + gesamtKosten + "€";
+    kostenDiv.innerHTML= "Gesamt Kosten: " + gesamtKosten + "€";
 
     zahl++;
 }
 
 function kaufen()
 {
+    if (guthaben>= gesamtKosten)
+    {
+        var meldung = document.getElementById("meldung");
+        meldung.style.backgroundColor="green"   
+        meldung.innerHTML=" Sie haben den Artikel gekauft!"
+        guthaben -= gesamtKosten;
+        document.getElementById("guthaben").innerHTML= "Guthaben:"+ "<b>" + guthaben + " €" + "</b>"
+    
+        gesamtKosten=0;
+        document.getElementById("gesamtKosten").innerHTML="Gesamt: 0";
 
+        zahl=1;
+        document.getElementById("warenkorb").innerHTML=""
+
+    }
+    else
+    {
+        var meldung = document.getElementById("meldung");
+        meldung.style.backgroundColor="red"   
+        meldung.innerHTML=" Sie haben nicht genügend Geld"
+    }
 }
