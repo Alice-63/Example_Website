f1b19dab334f5046d36ca15aade2e62f5c48544e
Alice-63 <alicetindag84@gmail.com>
2021-05-22 09:31:42 +0200

    Ali

--- script.js
+++ script.js
@@ -15,6 +15,13 @@ function hinzufuegen(produkt, preis)
     kostenDiv.innerHTML= "Gesamt Kosten: " + gesamtKosten + "€";
 
     zahl++;
+    var wk = document.getElementById("warenkorbNEU");
+
+    if(getComputedStyle(wk, null).display == "none")
+    {
+        wk.style.display="block";
+    }
+  
 }
 
 function kaufen()
@@ -24,14 +31,14 @@ function kaufen()
         var meldung = document.getElementById("meldung");
         meldung.style.backgroundColor="green"   
         meldung.innerHTML=" Sie haben den Artikel gekauft!"
+        
         guthaben -= gesamtKosten;
-        document.getElementById("guthaben").innerHTML= "Guthaben:"+ "<b>" + guthaben + " €" + "</b>"
-    
-        gesamtKosten=0;
-        document.getElementById("gesamtKosten").innerHTML="Gesamt: 0";
+    document.getElementById("guthaben").innerHTML= "Guthaben:"+ "<b>" + guthaben + " €" + "</b>"
 
-        zahl=1;
-        document.getElementById("warenkorb").innerHTML=""
+
+        reset();
+
+
 
     }
     else
@@ -40,4 +47,28 @@ function kaufen()
         meldung.style.backgroundColor="red"   
         meldung.innerHTML=" Sie haben nicht genügend Geld"
     }
+}
+
+function warenkorbShow ()
+{
+    var wk = document.getElementById("warenkorbNEU");
+
+    if(getComputedStyle(wk, null).display == "none")
+    {
+        wk.style.display="block";
+    }
+    else
+    {
+        wk.style.display="none";
+    }
+}
+
+function reset()
+{
+    
+    gesamtKosten=0;
+    document.getElementById("gesamtKosten").innerHTML="Gesamt: 0";
+
+    zahl=1;
+    document.getElementById("warenkorb").innerHTML=""
 }
